24f06602f364cc177a1843870926521a260e777d
Vasilii Lysenkov <vlysenkov@griddynamics.com>
2021-11-28 20:12:33 +0300

    added artifact

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -19,6 +19,8 @@ public class CardDeliveryTest {
         System.out.println(data);
 
 
+
+
         EstablishedDate firstDate = new EstablishedDate();
         EstablishedDate secondDate = new EstablishedDate();
         firstDate.setBankVisitDate(4);
